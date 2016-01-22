516f1be921207d134a1d4d6de1b6b675175cd9d3
Scott Lamb <slamb@slamb.org>
2016-01-22 00:39:44 -0800

    Add some polish.
     * add unit tests (with good test coverage).
       I refactored everything into a class in the process.
    
     * add a simple progress bar.
    
     * make ctrl-C work properly.
       I did this by getting rid of multithreading in favor of select().
       Signalling another thread would probably involve select()
       anyway (rather than sending something to the same socket a client would
       use, which seems gross), so this was the simplest thing to do.

--- hikvision_tftpd.py
+++ hikvision_tftpd.py
@@ -21,116 +21,155 @@ See discussion thread:
 https://www.ipcamtalk.com/showthread.php/3647-Hikvision-DS-2032-I-Console-Recovery
 """
 
+from __future__ import division
+
 __author__ = 'Scott Lamb'
 __license__ = 'MIT'
 __email__ = 'slamb@slamb.org'
 
 import errno
 import os
+import select
 import socket
 import struct
 import sys
-import threading
 import time
 
-_HANDSHAKE_BYTES = struct.pack('20s', 'SWKH')
-_CLIENT_IP = '192.0.0.64'
+HANDSHAKE_BYTES = struct.pack('20s', 'SWKH')
 _SERVER_IP = '192.0.0.128'
-_HANDSHAKE_CLIENT_ADDR = (_CLIENT_IP, 9979)
 _HANDSHAKE_SERVER_ADDR = (_SERVER_IP, 9978)
 _TFTP_SERVER_ADDR = (_SERVER_IP, 69)
-
 _FILENAME = 'digicap.dav'
+_TIME_FMT = '%T'
+_PROGRESS_WIDTH = 50
 
-# See https://tools.ietf.org/html/rfc1350
-_TFTP_OPCODE_RRQ = 1
-_TFTP_OPCODE_DATA = 3
-_TFTP_OPCODE_ACK = 4
-_TFTP_RRQ_PREFIX = struct.pack('>h', _TFTP_OPCODE_RRQ) + _FILENAME + '\x00'
-_TFTP_ACK_PREFIX = struct.pack('>h', _TFTP_OPCODE_ACK)
-_BLOCK_SIZE = 512
 
-def handshake_loop(sock):
-    while True:
-        pkt, addr = sock.recvfrom(len(_HANDSHAKE_BYTES))
-        now = time.strftime('%F %T')
-        if pkt == _HANDSHAKE_BYTES and addr == _HANDSHAKE_CLIENT_ADDR:
-            sock.sendto(pkt, addr)
+class Error(Exception): pass
+
+
+class Server(object):
+    # See https://tools.ietf.org/html/rfc1350
+    _TFTP_OPCODE_RRQ = 1
+    _TFTP_OPCODE_DATA = 3
+    _TFTP_OPCODE_ACK = 4
+    _TFTP_RRQ_PREFIX = struct.pack('>h', _TFTP_OPCODE_RRQ) + _FILENAME + '\x00'
+    _TFTP_ACK_PREFIX = struct.pack('>h', _TFTP_OPCODE_ACK)
+    BLOCK_SIZE = 512
+
+    def __init__(self, handshake_addr, tftp_addr, file_contents):
+        self._file_contents = file_contents
+        self._total_blocks = ((len(file_contents) + self.BLOCK_SIZE)
+                              // self.BLOCK_SIZE)
+        if self._total_blocks > 65535:
+            raise Error('File is too big to serve with %d-byte blocks.'
+                        % self.BLOCK_SIZE)
+        self._handshake_sock = self._bind(handshake_addr)
+        self._tftp_sock = self._bind(tftp_addr)
+        print 'Serving %d-byte %s (block size %d, %d blocks)' % (
+            len(file_contents), _FILENAME, self.BLOCK_SIZE, self._total_blocks)
+
+    def _bind(self, addr):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            sock.bind(addr)
+        except socket.error, e:
+            if e.errno == errno.EADDRNOTAVAIL:
+                raise Error(
+                    ('Address %s:%d not available.\n\n'
+                     'Try running:\n'
+                     'linux$ ifconfig eth0:0 192.0.0.64\n'
+                     'osx$   ifconfig en0 alias 192.0.0.64 255.255.255.0\n\n'
+                     '(adjust eth0 or en0 to taste. see "ifconfig -a" output)')
+                    % addr)
+            if e.errno == errno.EADDRINUSE:
+                raise Error(
+                    ('Address %s:%d in use.\n'
+                     'Make sure no other TFTP server is running.') % addr)
+            if e.errno == errno.EACCES:
+                raise Error(('No permission to bind to %s:%d.\n'
+                             'Try running with sudo.') % addr)
+            raise
+        return sock
+
+    def close(self):
+        self._handshake_sock.close()
+        self._tftp_sock.close()
+
+    def run_forever(self):
+        while True:
+            self._iterate()
+
+    def _iterate(self):
+        r, _, _ = select.select(
+            [self._handshake_sock, self._tftp_sock], [], [])
+        if self._handshake_sock in r:
+            self._handshake_read()
+        if self._tftp_sock in r:
+            self._tftp_read()
+
+    def _handshake_read(self):
+        pkt, addr = self._handshake_sock.recvfrom(len(HANDSHAKE_BYTES))
+        now = time.strftime(_TIME_FMT)
+        if pkt == HANDSHAKE_BYTES:
+            self._handshake_sock.sendto(pkt, addr)
             print '%s: Replied to magic handshake request.' % now
         else:
             print '%s: received unexpected bytes %r from %s:%d' % (
                 now, pkt.encode('hex'), addr[0], addr[1])
 
-def tftp_after(sock, file_contents, prev_block, addr):
-    block = prev_block + 1
-    start_byte = prev_block * _BLOCK_SIZE
-    if start_byte > len(file_contents):
-        return
-    block_data = file_contents[start_byte:start_byte+_BLOCK_SIZE]
-    pkt = (struct.pack('>hH', _TFTP_OPCODE_DATA, block) + block_data)
-    sock.sendto(pkt, addr)
-    now = time.strftime('%F %T')
-    print '%s: sending block %d (%d bytes%s)' % (
-        now, block, len(block_data),
-        ', done' if len(block_data) < _BLOCK_SIZE else '')
-
-def tftp_loop(sock, file_contents):
-    # TODO: This should retry DATA if an ACK isn't received soon enough.
-    # For now, it's entirely reactive.
-    while True:
-        pkt, addr = sock.recvfrom(len(_HANDSHAKE_BYTES))
-        now = time.strftime('%F %T')
-        if pkt.startswith(_TFTP_RRQ_PREFIX):
+    def _tftp_read(self):
+        try:
+            pkt, addr = self._tftp_sock.recvfrom(65536)
+        except socket.error, e:
+            if e.errno == e.ENOTCONN:
+                # socket was shutdown, as in shutdown().
+                return
+            raise
+        now = time.strftime(_TIME_FMT)
+        if pkt.startswith(self._TFTP_RRQ_PREFIX):
             print '%s: starting transfer' % now
-            tftp_after(sock, file_contents, 0, addr)
-        elif pkt.startswith(_TFTP_ACK_PREFIX):
-            (block,) = struct.unpack('>H', pkt[len(_TFTP_ACK_PREFIX):])
-            print '%s: received acknowledgement for block %d' % (now, block)
-            tftp_after(sock, file_contents, block, addr)
+            self._tftp_maybe_send(0, addr)
+        elif pkt.startswith(self._TFTP_ACK_PREFIX):
+            (block,) = struct.unpack(
+                '>H', pkt[len(self._TFTP_ACK_PREFIX):])
+
+            print '%s: acked %5d / %5d %s' % (
+                    now, block, self._total_blocks,
+                    '*' * (_PROGRESS_WIDTH * block // self._total_blocks))
+            self._tftp_maybe_send(block, addr)
         else:
             print '%s: received unexpected bytes %r from %s:%d' % (
                 now, pkt.encode('hex'), addr[0], addr[1])
 
-handshake_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-try:
-    handshake_sock.bind(_HANDSHAKE_SERVER_ADDR)
-except socket.error, e:
-    print 'Error: can\'t bind to %s:%d.' % _HANDSHAKE_SERVER_ADDR
-    if e.errno == errno.EADDRNOTAVAIL:
-        print 'IP address is not set up.'
-        print 'Try running: $ sudo ifconfig eth0:0 %s' % (
-            _HANDSHAKE_SERVER_ADDR[0])
-        sys.exit(1)
-    raise
-tftp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-try:
-    tftp_sock.bind(_TFTP_SERVER_ADDR)
-except socket.error, e:
-    print 'Error: can\'t bind to %s:%d.' % _TFTP_SERVER_ADDR
-    if e.errno == errno.EACCES:
-        print 'Please run with CAP_SYS_ADMIN or as root.'
-        sys.exit(1)
-    if e.errno == errno.EADDRINUSE:
-        print 'There seems to be another TFTP server running.'
-        sys.exit(1)
-    raise
-try:
-    file_contents = open(_FILENAME, 'r').read()
-except IOError, e:
-    print 'Error: can\'t read %s' % _FILENAME
-    if e.errno == errno.ENOENT:
-        print 'Please download/move it to the current working directory.'
-        sys.exit(1)
-    raise
+    def _tftp_maybe_send(self, prev_block, addr):
+        block = prev_block + 1
+        start_byte = prev_block * self.BLOCK_SIZE
+        if start_byte > len(self._file_contents):
+            return
+        block_data = self._file_contents[start_byte:start_byte+self.BLOCK_SIZE]
+        pkt = (struct.pack('>hH', self._TFTP_OPCODE_DATA, block) + block_data)
+        self._tftp_sock.sendto(pkt, addr)
+        now = time.strftime(_TIME_FMT)
+        print '%s: block %5d / %5d %s' % (
+                now, block, self._total_blocks,
+                '*' * (_PROGRESS_WIDTH * block // self._total_blocks))
 
-_TOTAL_BLOCKS = (len(file_contents) + _BLOCK_SIZE) // _BLOCK_SIZE
-if _TOTAL_BLOCKS > 65535:
-  print 'Error: file is too big to serve with %d-byte blocks.' % _BLOCK_SIZE
-  sys.exit(1)
-print 'Serving %d-byte %s (block size %d, %d blocks)' % (
-    len(file_contents), _FILENAME, _BLOCK_SIZE, _TOTAL_BLOCKS)
 
-handshake_thread = threading.Thread(target=handshake_loop,
-                                    args=(handshake_sock,))
-handshake_thread.start()
-tftp_loop(tftp_sock, file_contents)
+if __name__ == '__main__':
+    try:
+        file_contents = open(_FILENAME, 'r').read()
+    except IOError, e:
+        print 'Error: can\'t read %s' % _FILENAME
+        if e.errno == errno.ENOENT:
+            print 'Please download/move it to the current working directory.'
+            sys.exit(1)
+        raise
+
+    try:
+        server = Server(_HANDSHAKE_SERVER_ADDR, _TFTP_SERVER_ADDR,
+                        file_contents)
+    except Error, e:
+        print 'Error: %s' % e.strerror
+        sys.exit(1)
+
+    server.run_forever()
---
+++ hikvision_tftpd_test.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+
+__author__ = 'Scott Lamb'
+__license__ = 'MIT'
+__email__ = 'slamb@slamb.org'
+
+
+import errno
+import hikvision_tftpd
+import socket
+import string
+import unittest
+
+
+class TftpdTest(unittest.TestCase):
+    # From a packet capture.
+    _TEST_RRQ = ('\x00\x01digicap.dav\x00'  # request file digicap.dav
+                 'octet\x00'                # mode octet
+                 'timeout\x005\x00'         # RFC 2349 timeout = 5 seconds
+                 'blksize\x001468\x00')     # RFC 2348 block size = 1458
+
+    _LARGE_BUFFER_SIZE = 65536
+
+    def setUp(self):
+        self._server = None
+
+    def tearDown(self):
+        if self._server is not None:
+            self._server.close()
+            self._handshake_client.close()
+            self._tftp_client.close()
+
+    def _setup(self, data):
+        self._server = hikvision_tftpd.Server(
+                ('127.0.0.1', 0), ('127.0.0.1', 0), data)
+        self._handshake_client = socket.socket(
+            socket.AF_INET, socket.SOCK_DGRAM)
+        self._handshake_client.connect(
+            self._server._handshake_sock.getsockname())
+        self._tftp_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._tftp_client.connect(self._server._tftp_sock.getsockname())
+
+        # These tests run self._server._iterate() and then expect data to
+        # be available immediately or not at all. But at least on OS X,
+        # non-blocking operations don't already receive data that's already
+        # been sent to this socket, so give some fudge time.
+        self._tftp_client.settimeout(0.1)
+
+    def _assert_no_data(self):
+        try:
+            d = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        except socket.timeout:
+            pass
+        else:
+            self.fail('expected nothing, got: %r' % d)
+
+    def test_eaddrinuse(self):
+        self._setup('')
+        try:
+            hikvision_tftpd.Server(self._server._handshake_sock.getsockname(),
+                                   self._server._tftp_sock.getsockname(),
+                                   '')
+        except hikvision_tftpd.Error, e:
+            self.assertTrue('in use' in e.message, 'Unexpected: %r' % e)
+        else:
+            self.fail('expected an error')
+
+    def test_eaddrnotavail(self):
+        try:
+            # 192.0.2.0/24 is "TEST-NET", according to RFC 5737.
+            # The local machine shouldn't have such an IP address.
+            # (Okay, according to the RFCs, it shouldn't be using 192.0.0.128
+            # either, but we do what we must.)
+            hikvision_tftpd.Server(('192.0.2.1', 0), ('192.0.2.1', 0), '')
+        except hikvision_tftpd.Error, e:
+            self.assertTrue('not available' in e.message, 'Unexpected: %r' % e)
+        else:
+            self.fail('expected an error')
+
+    def test_eaccess(self):
+        try:
+            # The test shouldn't be running as root.
+            hikvision_tftpd.Server(('127.0.0.1', 1), ('127.0.0.1', 3), '')
+        except hikvision_tftpd.Error, e:
+            self.assertTrue('permission' in e.message, 'Unexpected: %r' % e)
+        else:
+            self.fail('expected an error. '
+                      '(did you run the tests as root? don\'t.)')
+
+
+    def test_proper_handshake(self):
+        self._setup('')
+        self._handshake_client.send(hikvision_tftpd.HANDSHAKE_BYTES)
+        self._server._iterate()
+        pkt = self._handshake_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual(hikvision_tftpd.HANDSHAKE_BYTES, pkt)
+
+    def test_bogus_handshake(self):
+        self._setup('')
+        self._handshake_client.send('asdf')
+        self._server._iterate()
+        self._assert_no_data()
+
+    def test_one_block(self):
+        data = string.letters
+        self._setup(data)
+        self._tftp_client.send(self._TEST_RRQ)
+        self._server._iterate()
+        pkt = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual('\x00\x03\x00\x01' + data, pkt)
+        self._tftp_client.send('\x00\x03\x00\x01')
+        self._server._iterate()
+        self._assert_no_data()
+
+    def test_two_block(self):
+        blocksize = hikvision_tftpd.Server.BLOCK_SIZE
+        repetitions = 1 + blocksize // len(string.letters)
+        data = string.letters * repetitions
+        self._setup(data)
+
+        # First packet.
+        self._tftp_client.send(self._TEST_RRQ)
+        self._server._iterate()
+        pkt = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual('\x00\x03\x00\x01' + data[:blocksize], pkt)
+
+        # Second packet.
+        self._tftp_client.send('\x00\x04\x00\x01')
+        self._server._iterate()
+        pkt = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual('\x00\x03\x00\x02' + data[blocksize:], pkt)
+
+        # No more packets.
+        self._tftp_client.send('\x00\x04\x00\x02')
+        self._server._iterate()
+        self._assert_no_data()
+
+    def test_full_block(self):
+        blocksize = hikvision_tftpd.Server.BLOCK_SIZE
+        data = 'x' * blocksize
+        self._setup(data)
+        self._tftp_client.send(self._TEST_RRQ)
+        self._server._iterate()
+        pkt = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual('\x00\x03\x00\x01' + data, pkt)
+
+        # Second packet (empty).
+        self._tftp_client.send('\x00\x04\x00\x01')
+        self._server._iterate()
+        pkt = self._tftp_client.recv(self._LARGE_BUFFER_SIZE)
+        self.assertEqual('\x00\x03\x00\x02', pkt)
+
+        # No more packets.
+        self._tftp_client.send('\x00\x04\x00\x02')
+        self._server._iterate()
+        self._assert_no_data()
+
+    def test_max_file_size(self):
+        # The number of blocks in the file must fit within 16 bits.
+        # The final block can't be full.
+        max_blocks = 2**16 - 1
+        max_size = hikvision_tftpd.Server.BLOCK_SIZE * max_blocks - 1
+
+        self._setup('x' * max_size)
+        self._server.close()
+        self.assertRaises(hikvision_tftpd.Error,
+                          self._setup, 'x' * (max_size + 1))
+
+
+if __name__ == '__main__':
+    unittest.main()
