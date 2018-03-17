7494754f1c548688fee95e2e5de42ef029a128b2
Josh <jancill@envysion.com>
2018-03-17 01:53:19 -0700

    Changes based on PR feedback.
    -Merged string constants
    -Fixed fuction spacing
    -Moved block_size to constructor
    -Set default block_size (512) after transfer is compelte
    -Add a missing error check if blksize is not used in read request and
    file size is too big for default the block size

--- hikvision_tftpd.py
+++ hikvision_tftpd.py
@@ -22,6 +22,7 @@ HANDSHAKE_BYTES = struct.pack('20s', 'SWKH')
 _HANDSHAKE_SERVER_PORT = 9978
 _TFTP_SERVER_PORT = 69
 _TIME_FMT = '%c'
+_DEFAULT_BLOCK_SIZE = 512
 
 
 class Error(Exception): pass
@@ -34,20 +35,20 @@ class Server(object):
     _TFTP_OPCODE_ACK = 4
     _TFTP_OPCODE_OACK = 6
     _TFTP_ACK_PREFIX = struct.pack('>h', _TFTP_OPCODE_ACK)
-    BLOCK_SIZE = 512
 
     def __init__(self, handshake_addr, tftp_addr, filename, file_contents):
         self._file_contents = file_contents
         self._filename = filename
-        self._total_blocks = ((len(file_contents) + self.BLOCK_SIZE)
-                              // self.BLOCK_SIZE)
+        self._block_size = _DEFAULT_BLOCK_SIZE
+        self._total_blocks = ((len(file_contents) + self._block_size)
+                              // self._block_size)
         self._tftp_rrq_prefix = (struct.pack('>h', self._TFTP_OPCODE_RRQ) +
                                  filename + '\x00')
-        self._tftp_blksize_option = ('blksize' + '\x00')
+        self._tftp_blksize_option = ('blksize\x00')
         self._handshake_sock = self._bind(handshake_addr)
         self._tftp_sock = self._bind(tftp_addr)
         print 'Serving %d-byte %s (block size %d, %d blocks)' % (
-            len(file_contents), filename, self.BLOCK_SIZE, self._total_blocks)
+            len(file_contents), filename, self._block_size, self._total_blocks)
 
     def _bind(self, addr):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -74,15 +75,24 @@ class Server(object):
         return sock
 
     def set_block_size(self, block_size):
+        # TODO: Don't mutate overall server for a single transfer. Use some kind of per-transfer state
         print 'Setting block size to %d' % block_size
-        self.BLOCK_SIZE = block_size
-        self._total_blocks = ((len(self._file_contents) + self.BLOCK_SIZE)
-                              // self.BLOCK_SIZE)
+        self._block_size = block_size
+        self._total_blocks = ((len(self._file_contents) + self._block_size)
+                              // self._block_size)
         if self._total_blocks > 65535:
             raise Error('File is too big to serve with %d-byte blocks.'
-                        % self.BLOCK_SIZE)
+                        % self._block_size)
         print 'Serving %d-byte %s (block size %d, %d blocks)' % (
-            len(self._file_contents), self._filename, self.BLOCK_SIZE, self._total_blocks)
+            len(self._file_contents), self._filename, self._block_size, self._total_blocks)
+
+    def set_default_block_size(self):
+        print 'Setting block size to %d' % _DEFAULT_BLOCK_SIZE
+        self._block_size = _DEFAULT_BLOCK_SIZE
+        self._total_blocks = ((len(self._file_contents) + self._block_size)
+                              // self._block_size)
+        print 'Serving %d-byte %s (block size %d, %d blocks)' % (
+            len(self._file_contents), self._filename, self._block_size, self._total_blocks)
 
     def close(self):
         self._handshake_sock.close()
@@ -121,6 +131,9 @@ class Server(object):
             print '%s: sending options ack' % now
             self._tftp_options_ack(addr)
         elif pkt.startswith(self._tftp_rrq_prefix):
+            if self._total_blocks > 65535:
+                raise Error('File is too big to serve with %d-byte blocks.'
+                            % self._block_size)
             print '%s: starting transfer' % now
             self._tftp_maybe_send(0, addr)
         elif pkt.startswith(self._TFTP_ACK_PREFIX):
@@ -130,17 +143,20 @@ class Server(object):
         else:
             print '%s: received unexpected tftp bytes %r from %s:%d' % (
                 now, pkt.encode('hex'), addr[0], addr[1])
+
     def _tftp_options_ack(self, addr):
-        pkt = (struct.pack('>H', self._TFTP_OPCODE_OACK) + 'blksize' + '\x00' + str(self.BLOCK_SIZE) + '\x00')
+        pkt = (struct.pack('>H', self._TFTP_OPCODE_OACK) + 'blksize\x00' + str(self._block_size) + '\x00')
         self._tftp_sock.sendto(pkt, addr)
 
     def _tftp_maybe_send(self, prev_block, addr):
         block = prev_block + 1
-        start_byte = prev_block * self.BLOCK_SIZE
+        start_byte = prev_block * self._block_size
         if start_byte > len(self._file_contents):
             print '%s: done!' % time.strftime(_TIME_FMT)
+            if self._block_size != _DEFAULT_BLOCK_SIZE:
+                self.set_default_block_size()
             return
-        block_data = self._file_contents[start_byte:start_byte+self.BLOCK_SIZE]
+        block_data = self._file_contents[start_byte:start_byte+self._block_size]
         pkt = (struct.pack('>hH', self._TFTP_OPCODE_DATA, block) + block_data)
         self._tftp_sock.sendto(pkt, addr)
         _progress_width = 53
